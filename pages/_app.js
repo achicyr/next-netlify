cf4902f38d3367ea78f2fefded6b16cf2d243022
webdev-archist <webdev.archist@gmail.com>
2021-12-10 10:31:50 +0100

    DONE	initiated project

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Link href="https://www.youtube.com/playlist?list=PL4cUxeGkcC9ig-veuRaLI4QB0Ws8xMzjv"><a>lien vers l aplaylist</a></Link>
       <Navbar />
       <Component {...pageProps} />
     </div>
